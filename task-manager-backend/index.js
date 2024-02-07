e5f7d694b702f694c78bd76df865310ab8b83570
Mayank thukral <79597046+mayank91091@users.noreply.github.com>
2024-02-06 21:20:15 -0500

    Update index.js

--- task-manager-backend/index.js
+++ task-manager-backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to Azure Cosmos DB for MongoDB
-mongoose.connect("mongodb+srv://mayankthukral1810:<password>@cluster0.zyjeuqn.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://mayankthukral1810:321hello@cluster0.zyjeuqn.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to Azure Cosmos DB');
   })
